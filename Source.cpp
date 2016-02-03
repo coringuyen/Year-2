db48aef923a78ecdd1a44874b13d63b885fab769
Matthew Williamson <chuwilliamson@gmail.com>
2016-02-03 17:20:31 -0600

    obj

--- Source.cpp
+++ Source.cpp
@@ -4,28 +4,112 @@
 #include <Gizmos.h>
 #include <glm\glm.hpp>
 #include <glm\ext.hpp>
-
+#include <vector>
 #include "MyApplication.h"
+
+namespace tinyobj
+{
+
+	typedef struct mesh_t
+	{
+		std::vector<float>			positions;
+		std::vector<float>			normals;
+		std::vector<float>			texcoords;
+		std::vector<unsigned int>	indices;
+		std::vector<int>			material_ids;
+	};
+
+	typedef struct shape_t
+	{
+		std::string			name;
+		mesh_t				mesh;
+	};
+
+
+}
+
+struct OpenGLInfo
+{
+	unsigned int m_VAO;
+	unsigned int m_VBO;
+	unsigned int m_IBO;
+	unsigned int m_index_count;
+};
+
+std::vector<OpenGLInfo> m_gl_info;
+void createOpenGLBuffers(std::vector<tinyobj::shape_t>& shapes)
+{
+	m_gl_info.resize(shapes.size());
+	for (unsigned int mesh_index = 0; mesh_index < shapes.size(); ++mesh_index) 
+	{
+		glGenVertexArrays(1, &m_gl_info[mesh_index].m_VAO);
+		glGenBuffers(1, &m_gl_info[mesh_index].m_VBO);
+		glGenBuffers(1, &m_gl_info[mesh_index].m_IBO);  
+		glBindVertexArray(m_gl_info[mesh_index].m_VAO);
+
+		unsigned int float_count = shapes[mesh_index].mesh.positions.size();  
+		float_count += shapes[mesh_index].mesh.normals.size(); 
+		float_count += shapes[mesh_index].mesh.texcoords.size();
+
+		std::vector<float> vertex_data;		
+		vertex_data.reserve(float_count);
+
+		vertex_data.insert(vertex_data.end(),
+			shapes[mesh_index].mesh.positions.begin(), 
+			shapes[mesh_index].mesh.positions.end()); 
+		
+		vertex_data.insert(vertex_data.end(), 
+			shapes[mesh_index].mesh.normals.begin(),
+			shapes[mesh_index].mesh.normals.end());
+		
+		m_gl_info[mesh_index].m_index_count = 
+			shapes[mesh_index].mesh.indices.size();
+
+		glBindBuffer(GL_ARRAY_BUFFER, m_gl_info[mesh_index].m_VBO);  
+		glBufferData(GL_ARRAY_BUFFER, 
+					vertex_data.size() * sizeof(float),
+					vertex_data.data(), GL_STATIC_DRAW);
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_gl_info[mesh_index].m_IBO);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, 
+					shapes[mesh_index].mesh.indices.size() * sizeof(unsigned int), 
+					shapes[mesh_index].mesh.indices.data(), GL_STATIC_DRAW);
+
+		glEnableVertexAttribArray(0); //position
+		glEnableVertexAttribArray(1); //normal data  
+		
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);  
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_TRUE, 0, 
+		(void*)(sizeof(float)*shapes[mesh_index].mesh.positions.size()));
+		
+		glBindVertexArray(0);  
+		glBindBuffer(GL_ARRAY_BUFFER, 0);  
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	}
+	
+}
 
 
 int main()
 {
-	Application *theApp = new Application();
-	
-	if(theApp->startup() == true) 
-	{
-		while (theApp->update() == true)
+	std::vector<tinyobj::shape_t> shapes;
+
+		Application *theApp = new Application();
+		
+		if(theApp->startup() == true) 
 		{
-			theApp->draw();
+			while (theApp->update() == true)
+			{
+				theApp->draw();
+			}
+			theApp->shutdown();
 		}
-		theApp->shutdown();
-	}
-	
-
- 
- 
 
 
-	//delete theApp;
+
+
+
+
+		delete theApp;
 	return 0;
 }
--- dep/include/Gizmos.h
+++ dep/include/Gizmos.h
@@ -39,34 +39,6 @@ public:
 	// at the transform's translation. Optional scale available.
 	static void		addTransform(const glm::mat4& a_transform, float a_fScale = 1.0f);
 	
-	// Adds a wireframe Axis-Aligned Bounding-Box with optional transform for rotation/translation.
-	static void		addAABB(const glm::vec3& a_center, const glm::vec3& a_extents, 
-							const glm::vec4& a_colour, const glm::mat4* a_transform = nullptr);
-
-	// Adds an Axis-Aligned Bounding-Box with optional transform for rotation.
-	static void		addAABBFilled(const glm::vec3& a_center, const glm::vec3& a_extents, 
-								  const glm::vec4& a_fillColour, const glm::mat4* a_transform = nullptr);
-
-	// Adds a cylinder aligned to the Y-axis with optional transform for rotation.
-	static void		addCylinderFilled(const glm::vec3& a_center, float a_radius, float a_fHalfLength,
-									  unsigned int a_segments, const glm::vec4& a_fillColour, const glm::mat4* a_transform = nullptr);
-
-	// Adds a double-sided hollow ring in the XZ axis with optional transform for rotation.
-	// If a_rvFilLColour.w == 0 then only an outer and inner line is drawn.
-	static void		addRing(const glm::vec3& a_center, float a_innerRadius, float a_outerRadius,
-							unsigned int a_segments, const glm::vec4& a_fillColour, const glm::mat4* a_transform = nullptr);
-
-	// Adds a double-sided disk in the XZ axis with optional transform for rotation.
-	// If a_rvFilLColour.w == 0 then only an outer line is drawn.
-	static void		addDisk(const glm::vec3& a_center, float a_radius,
-							unsigned int a_segments, const glm::vec4& a_fillColour, const glm::mat4* a_transform = nullptr);
-
-	// Adds an arc, around the Y-axis
-	// If a_rvFilLColour.w == 0 then only an outer line is drawn.
-	static void		addArc(const glm::vec3& a_center, float a_rotation,
-						   float a_radius, float a_halfAngle,
-						   unsigned int a_segments, const glm::vec4& a_fillColour, const glm::mat4* a_transform = nullptr);
-
 	// Adds an arc, around the Y-axis, starting at the inner radius and extending to the outer radius
 	// If a_rvFilLColour.w == 0 then only an outer line is drawn.
 	static void		addArcRing(const glm::vec3& a_center, float a_rotation, 
@@ -78,17 +50,6 @@ public:
 							  const glm::mat4* a_transform = nullptr, float a_longMin = 0.f, float a_longMax = 360, 
 							  float a_latMin = -90, float a_latMax = 90 );
 
-	// Adds a single Hermite spline curve
-	static void		addHermiteSpline(const glm::vec3& a_start, const glm::vec3& a_end,
-									 const glm::vec3& a_tangentStart, const glm::vec3& a_tangentEnd, unsigned int a_segments, const glm::vec4& a_colour);
-
-	// 2-dimensional gizmos
-	static void		add2DLine(const glm::vec2& a_start, const glm::vec2& a_end, const glm::vec4& a_colour);
-	static void		add2DLine(const glm::vec2& a_start, const glm::vec2& a_end, const glm::vec4& a_colour0, const glm::vec4& a_colour1);	
-	static void		add2DTri(const glm::vec2& a_0, const glm::vec2& a_1, const glm::vec2& a_2, const glm::vec4& a_colour);	
-	static void		add2DAABB(const glm::vec2& a_center, const glm::vec2& a_extents, const glm::vec4& a_colour, const glm::mat4* a_transform = nullptr);	
-	static void		add2DAABBFilled(const glm::vec2& a_center, const glm::vec2& a_extents, const glm::vec4& a_colour, const glm::mat4* a_transform = nullptr);	
-	static void		add2DCircle(const glm::vec2& a_center, float a_radius, unsigned int a_segments, const glm::vec4& a_colour, const glm::mat4* a_transform = nullptr);
 	
 private:
 
